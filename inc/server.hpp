5cd7881ceecef1d51ce407acf06eb9376015e998
seaflower <mikeqoo123@gmail.com>
2021-12-06 15:56:50 +0800

    完整板

--- inc/server.hpp
+++ inc/server.hpp
@@ -17,6 +17,17 @@
 #include <iostream>
 
 using namespace std;
+#define BUFFER_LEN 1024
+#define NAME_LEN 20       //User Name 長度
+#define MAX_CLIENT_NUM 32 //最大連線數
+
+struct User_Client //紀錄使用者資訊
+{
+    int valid;               //to judge whether this user is online
+    int fd_id;               //user ID number
+    int socket;              //socket to this user
+    char name[NAME_LEN + 1]; //name of the user
+};
 
 class Server
 {
@@ -31,20 +42,25 @@ public:
     ~Server() {}
 
 public:
-    int serverBind();
-    //maxbacklog指定監聽佇列大小，當有連線請求到來會進入此監聽佇列，連線請求被 accept() 後會離開監聽佇列，當佇列滿時，新的連線請求會返回錯誤
-    int serverListen(int maxbacklog);
-    void serverAccept();
-    int serverSendAll(vector<int> &, char *sendBuf);
-    void thread_func(int connfd);
-    void servRun();
+    int servRun();
+    void *chat(User_Client *data);
+    void handle_recv(void *data);
+    void *handle_send(User_Client *data); //send message
 
 private:
     int sockfd;
-    int clientid;
+    int current_client_num;
     struct sockaddr_in serverIP;
-    vector<int> conn_list;
+    User_Client client[MAX_CLIENT_NUM] = {0};
+    queue<string> message_q[MAX_CLIENT_NUM]; //message buffer
     std::mutex my_lock;
+    std::mutex my_lock_client[MAX_CLIENT_NUM];      //給client用的鎖
+    std::condition_variable notify[MAX_CLIENT_NUM]; //條件變數
+    std::thread my_chat_thread[MAX_CLIENT_NUM];
+    std::thread my_send_thread[MAX_CLIENT_NUM];
 };
+
+//https://shengyu7697.github.io/cpp11-how/
+//條件變數用來通知各個thread https://baike.baidu.hk/item/pthread_cond_signal/564029
 
 #endif /* SERVER_HPP */
